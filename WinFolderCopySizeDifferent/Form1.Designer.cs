b29152366694cfb99cc4d0ecd37e30240fe42333
주민규 <minggoo@msn.com>
2019-08-27 15:32:30 +0900

    commit

--- WinFolderCopySizeDifferent/Form1.Designer.cs
+++ WinFolderCopySizeDifferent/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.rtxtResult = new System.Windows.Forms.RichTextBox();
+            this.txtDestinationDir = new System.Windows.Forms.TextBox();
+            this.txtSourceDir = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnDestinationDir = new System.Windows.Forms.Button();
+            this.btnSourceDir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(75, 428);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // rtxtResult
+            // 
+            this.rtxtResult.Location = new System.Drawing.Point(75, 65);
+            this.rtxtResult.Name = "rtxtResult";
+            this.rtxtResult.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.rtxtResult.Size = new System.Drawing.Size(651, 357);
+            this.rtxtResult.TabIndex = 12;
+            this.rtxtResult.Text = "";
+            // 
+            // txtDestinationDir
+            // 
+            this.txtDestinationDir.Location = new System.Drawing.Point(75, 32);
+            this.txtDestinationDir.Name = "txtDestinationDir";
+            this.txtDestinationDir.Size = new System.Drawing.Size(527, 21);
+            this.txtDestinationDir.TabIndex = 11;
+            // 
+            // txtSourceDir
+            // 
+            this.txtSourceDir.Location = new System.Drawing.Point(75, -1);
+            this.txtSourceDir.Name = "txtSourceDir";
+            this.txtSourceDir.Size = new System.Drawing.Size(527, 21);
+            this.txtSourceDir.TabIndex = 10;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(646, 428);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(80, 23);
+            this.btnCopy.TabIndex = 9;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnDestinationDir
+            // 
+            this.btnDestinationDir.Location = new System.Drawing.Point(608, 31);
+            this.btnDestinationDir.Name = "btnDestinationDir";
+            this.btnDestinationDir.Size = new System.Drawing.Size(118, 23);
+            this.btnDestinationDir.TabIndex = 8;
+            this.btnDestinationDir.Text = "DestinationFolder";
+            this.btnDestinationDir.UseVisualStyleBackColor = true;
+            // 
+            // btnSourceDir
+            // 
+            this.btnSourceDir.Location = new System.Drawing.Point(608, -1);
+            this.btnSourceDir.Name = "btnSourceDir";
+            this.btnSourceDir.Size = new System.Drawing.Size(118, 23);
+            this.btnSourceDir.TabIndex = 7;
+            this.btnSourceDir.Text = "SourceFolder";
+            this.btnSourceDir.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 496);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.rtxtResult);
+            this.Controls.Add(this.txtDestinationDir);
+            this.Controls.Add(this.txtSourceDir);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnDestinationDir);
+            this.Controls.Add(this.btnSourceDir);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RichTextBox rtxtResult;
+        private System.Windows.Forms.TextBox txtDestinationDir;
+        private System.Windows.Forms.TextBox txtSourceDir;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnDestinationDir;
+        private System.Windows.Forms.Button btnSourceDir;
     }
 }
 
--- WinFolderCopySizeDifferent/Form1.cs
+++ WinFolderCopySizeDifferent/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,98 @@ namespace WinFolderCopySizeDifferent
 {
     public partial class Form1 : Form
     {
+        FileInfo sourceFile;
+        FileInfo targetFile;
+        DirectoryInfo dirInfo;
+
+        string sourceDir = String.Empty;
+        string targetDir = String.Empty;
+        int sleepTime = 500;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            //CopyOverwriteWithDifferentSize(sourceDir);
+            Thread t1 = new Thread(new ThreadStart(ThreadWorker));
+            t1.Start();
+            //ThreadWorker();
+        }
+
+        #region "Utility"
+        private void Log(string txtLog)
+        {
+            //rtxtLoginMessage.Select(0, 0);
+            //rtxtLoginMessage.SelectedText = line + Environment.NewLine;
+            if (rtxtResult.InvokeRequired)
+            {
+                rtxtResult.BeginInvoke(new Action(() => rtxtResult.Select(0, 0)));
+                rtxtResult.BeginInvoke(new Action(() => rtxtResult.SelectedText = txtLog + "\r\n"));
+            }
+            else
+            {
+                rtxtResult.Select(0, 0);
+                rtxtResult.SelectedText = txtLog + "\r\n";
+            }
+            //this.Refresh();
+        }
+        #endregion
+
+        #region "main oeperation"
+
+        private void ThreadWorker()
+        {
+            CopyOverwriteWithDifferentSize(sourceDir);
+        }
+
+        private void CopyOverwriteWithDifferentSize(string dirParent)
+        {
+            foreach (var dir in Directory.GetDirectories(dirParent))
+            {
+                dirInfo = new DirectoryInfo(dir);
+                // files
+                foreach (var file in Directory.GetFiles(dir))
+                {
+                    sourceFile = new FileInfo(file);
+                    if (sourceFile.Length > 0)
+                    {
+                        targetFile = new FileInfo(Path.Combine(Path.GetDirectoryName(sourceFile.FullName).Replace(sourceDir, targetDir), Path.GetFileName(file)));
+                        if (targetFile.Exists)
+                        {
+                            if (sourceFile.Length < targetFile.Length)
+                            {
+                                File.Copy(file, targetFile.FullName, true);
+                                //Console.WriteLine(targetFile.FullName);
+                                Log(targetFile.FullName);
+                                Thread.Sleep(sleepTime);
+                            }
+                        }
+                        else
+                        {
+                            string targetDirName = Path.GetDirectoryName(targetFile.FullName);
+                            DirectoryInfo di = new DirectoryInfo(targetDirName);
+
+                            if (!di.Exists)
+                                Directory.CreateDirectory(targetDirName);
+
+                            File.Copy(file, targetFile.FullName, true);
+                            //Console.WriteLine(targetFile.FullName);
+                            Log(targetFile.FullName);
+                            Thread.Sleep(sleepTime);
+                        }
+                    }
+                }
+                // sub directories
+                if (dirInfo.GetDirectories().Length > 0)
+                    CopyOverwriteWithDifferentSize(dirInfo.FullName);
+
+                //this.Refresh();
+            }
+            Log("END - " + dirParent);
+        }
+        #endregion
+
     }
 }
